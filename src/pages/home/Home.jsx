71c19aaf5da2463d51409918b9f64f04ea1e7242
Peter-Kibet <peter.kibet@hotmail.com>
2023-02-11 09:20:11 +0300

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Home.scss"
+const Home = () => {
+  return (
+    <div class="home">
+        <div className="homeWrapper"></div>
+    </div>
+  );
+};
+
+export default Home
